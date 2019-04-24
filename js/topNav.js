52a988c814024ce4f4c1d211defdf2ecf5f2dc92
thefitz7 <s.fitzpatrick.9977@gmail.com>
2019-04-24 10:48:41 -0400

    Updating basics

---
+++ js/topNav.js
@@ -0,0 +1,8 @@
+function topnav_click() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
